ad5ee9d3e8dc9068b8e59422cb5002e42291f11d
Zachary Fong <fong.zachary@gmail.com>
2016-08-26 15:46:27 -0700

    separate keypress for each car

--- app.js
+++ app.js
@@ -2,17 +2,40 @@
 $(document).ready(function() {
   console.log("js works!");
 
+  //var car1left = $('#car1').css('margin-left');
+  //var car2left = $('#car2').css('margin-left');
+
   $(document).keypress(function(event) {
-    //if(event.which === 39){
+    var car1left = $('#car1').css('margin-left');
+    var car2left = $('#car2').css('margin-left');
+
+    //on 'D' press
+    if(event.keyCode === 100){
       $('#car1').css('margin-left', '+=30px');
       console.log($('#car1').css('margin-left'));
-    //};
+      console.log(event.keyCode);
+      if(car1left === "930px"){
+        $('#score').append("car1wins");
+
+      }
+    };
+
+    //on 'L' press
+    if(event.keyCode === 108){
+      $('#car2').css('margin-left', '+=30px');
+      console.log(car2left);
+      console.log(event.keyCode);
+      if(car2left === "930px"){
+        $('#score').append("car2wins");
+      }
+    };
   });
 
-  // $(document).keypress(function() {
-  //   $('#car2').css('margin-left', '+=30px');
-  //   console.log($('#car2').css('margin-left'))
-  // });
 
+
+
+  //if(car1left === ){
+
+  //}
 
 })
